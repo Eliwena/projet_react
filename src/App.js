bf2980169260fc9a8713e3c68efc82d0cb100bab
Océane <46958330+Eliwena@users.noreply.github.com>
2021-01-11 17:19:49 +0100

    zero

--- src/App.js
+++ src/App.js
@@ -1,33 +1,10 @@
-import React from "react";
+import logo from './logo.svg';
+import './App.css';
 
-import App_poke from "./pokemon_app"
-import Index from "./home"
-import App_meteo from "./meteo_app";
-
-const show_poke = () => {
-  if (window.location.pathname === "/pokemon_app") {
-    return <App_poke></App_poke>
-  }
-}
-
-const show_meteo = () => {
-  if (window.location.pathname === "/meteo_app") {
-    return <App_meteo></App_meteo>
-  }
-}
-
-const show_index = () => {
-  if (window.location.pathname === "/") {
-    return <Index></Index>
-  }
-}
-
-export default () => {
+function App() {
   return (
-    <div className="ui container">
-      {show_poke()}
-      {show_meteo()}
-      {show_index()}
-    </div>
-  )
+      <div>Bonjour</div>
+  );
 }
+
+export default App;

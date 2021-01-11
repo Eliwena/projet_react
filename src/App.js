27533681a216ea6c06948f3426bbf24ed2e51e16
Océane <46958330+Eliwena@users.noreply.github.com>
2021-01-11 17:23:47 +0100

    add new file + meteo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-      <div>Bonjour</div>
-  );
+import App_poke from "./pokemon_app"
+import Index from "./home"
+import App_meteo from "./meteo_app";
+
+const show_poke = () => {
+  if (window.location.pathname === "/pokemon_app") {
+    return <App_poke></App_poke>
+  }
 }
 
-export default App;
+const show_meteo = () => {
+  if (window.location.pathname === "/meteo_app") {
+    return <App_meteo></App_meteo>
+  }
+}
+
+const show_index = () => {
+  if (window.location.pathname === "/") {
+    return <Index></Index>
+  }
+}
+
+export default () => {
+  return (
+    <div className="ui container">
+      {show_poke()}
+      {show_meteo()}
+      {show_index()}
+    </div>
+  )
+}
